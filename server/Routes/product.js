c24a1d26958f360f37ca881f92b5edf15738893d
kelly benson <kellyjbenson@gmail.com>
2019-09-24 08:18:37 -0500

    tokens working

--- server/Routes/product.js
+++ server/Routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const models = require('../models')
+router.use(express.json())
 
 
 //add product
@@ -22,9 +23,40 @@ router.post('/add-product', (req, res) => {
     })
 })
 
-//view all products
+//view all products 
+router.get('/', (req, res) => {
+    models.Product.findAll().then(products =>{
+        res.json(products)
+    } )
+})
+
 
 //update product
+router.post('/update-product', (req, res) => {
+    //need to add user authentication
+    const id = req.body.id
+    const product_name = req.body.product_name
+    const product_qty = req.body.product_qty
+    const product_type = req.body.product_type
+    const product_image = req.body.product_image
+    const product_description = req.body.product_description
+    const user_description = req.body.user_description
+    const lisitng_expiration = req.body.lisitng_expiration
+    const user_id = req.body.user_id
+    
+    let product = models.Product.update({
+        product_name, product_qty, product_type, product_image, product_description, user_description, lisitng_expiration, user_id
+
+    },{
+        where: {
+            id: id
+        }
+    })
+    .then(() => {
+        res.send("Update successful!")
+    }).catch(e=>console.log(e))
+})
+
 
 //delete product
 router.delete('/delete', (req, res) => {
@@ -37,6 +69,18 @@ router.delete('/delete', (req, res) => {
     res.send("Deleted!")
 })
 
+//sort product by type
+router.get('/product-sort', (req, res) => {
+    let type = req.body.product_type
+    models.Product.findAll({
+        where: {
+            product_type: type
+        }
+    }).then(products => {
+        res.json(products)
+    })
+})
 
+//keyword search
 
 module.exports = router
